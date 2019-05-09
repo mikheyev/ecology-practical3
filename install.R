5a2f2c4e2c22503860d2c0bcb5e2733bb979f2e5
Sasha Mikheyev <sasha [at] homologo.us>
2019-05-09 23:13:12 +1000

    added devtools

--- install.R
+++ install.R
@@ -1,5 +1,6 @@
 
-install.packages(c("tidyverse", "devtools"))
+install.packages("tidyverse")
+install.packages()"devtools")
+devtools::install_github("vqv/ggbiplot")
 install.packages("IRkernel")
 IRkernel::installspec()
-devtools::install_github("vqv/ggbiplot")
